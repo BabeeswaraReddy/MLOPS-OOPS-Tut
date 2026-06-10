f31becffa0bee9865dccb1a1969a1586efc77c21
babeeswarareddyirala <babeeswarareddyirala@gmail.com>
2026-06-10 20:01:52 +0530

    Encapsulation, getter and setter and static method covered

--- oops1.py
+++ oops1.py
@@ -2,16 +2,21 @@
 class Employee:
     # special method/magic method/dunder method - contructor
     def __init__(self):
-         
+        print(id(self))
+        #print("Started executing attribute/data") 
         self.id = 123
         self.salary = 50000
         self.designation = "SDE"
         
-    def travel(self,destination):
-        print(f"Traveling to {destination}")
+    def travel(self):
+        print(f"Traveling to Delhi")
         
 # create an object of the class
 Ram = Employee()
+print(id(Ram))
 
-print(Ram.designation)
-Ram.travel("New York")
+shaktiman = Employee()
+print(id(shaktiman))
+
+#print(Ram.designation)
+#Ram.travel("New York")
--- rough.py
+++ rough.py
@@ -1,3 +1,27 @@
 from oops_proj import chatbook
-
 user1 = chatbook()
+print(user1.id)
+
+
+#Using static method directly from class rather than obj
+#chatbook.set_id(100)
+#user2 = chatbook()
+#print(user2.id)
+
+#user3 = chatbook()
+#print(user3.id)
+# getter and setter
+#print(user1.get_name())
+#user1.set_name("Rohit")
+#print(user1.get_name())
+
+# #  function vs method
+#lst=[1,2,3]
+
+# # function
+#a1 = len(lst)
+#print(a1)
+
+#method
+#user1 = chatbook()
+#user1.send_message()
